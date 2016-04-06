7d0422b3da0453e3a824a3898ad5e529e9e30dfd
Claire Palmer <clairepalmer@wireless-10-147-228-65.public.utexas.edu>
2016-04-06 16:42:59 -0500

    Minor changes to format.

--- labelrna2.0/bpseq_to_alps.py
+++ labelrna2.0/bpseq_to_alps.py
@@ -26,38 +26,50 @@ def bpseq_to_alps():
 	#Add columns
 	alden.insert(3, "ie", np.nan)
 	alden.insert(3, "ps", np.nan)
-	alden.insert(3, "label", np.nan)
+	alden.insert(3, "label2", np.nan)
+	alden.insert(3, "label1", np.nan)
 
 	#Define primary helices
 	hairpins = alden[alden[2] == "HAIRPIN"]
 	helix = 1
 	for row in hairpins.itertuples():
+		alden.ix[row[0], "label1"] = 'P'+str(helix)
 		up = row[0] + 1 
 		down = row[0] - 1
-		letter = 1
+		letter = 0
 		#initiation
 		i = alden.ix[up, 3]
 		alden.ix[alden[3]==i, "ps"] = "PRIMARY"
 		alden.ix[alden[3]==i, "ie"] = "INITIATION"
-		alden.ix[alden[3]==i, "label"] = 'P'+str(helix)+'a'
+		alden.ix[alden[3]==i, "label1"] = 'P'+str(helix)
+		if row[1] == row[2]:
+			alden.ix[alden[3]==i, "label2"] = 'x'
+		else:
+			alden.ix[alden[3]==i, "label2"] = ascii_lowercase[letter]
+			letter += 1
 		#elongation
 		while up < len(alden.index) and alden.ix[up, 2] != "MULTISTEM" and alden.ix[up, 2] != "FREE" and down > 0 and alden.ix[down, 2] != "MULTISTEM" and alden.ix[down, 2] != "FREE":
 			if alden.ix[up, 2] == "BULGE":
+				i = alden.ix[up, 3]
+				alden.ix[alden[3]==i, "label1"] = 'P'+str(helix)
 				up += 1
 			if alden.ix[down, 2] == "BULGE":
+				i = alden.ix[down, 3]
+				alden.ix[alden[3]==i, "label1"] = 'P'+str(helix)
 				down -= 1
+			if alden.ix[up, 2] == "INTERNAL":
+				i = alden.ix[up, 3]
+				alden.ix[alden[3]==i, "label1"] = 'P'+str(helix)
 			if alden.ix[up, 2] == "HELIX" and alden.ix[up, "ie"] != "INITIATION" and alden.ix[up, "ie"] != "ELONGATION":
 				i = alden.ix[up, 3]
 				alden.ix[alden[3]==i, "ps"] = "PRIMARY"
 				alden.ix[alden[3]==i, "ie"] = "ELONGATION"
-				alden.ix[alden[3]==i, "label"] = 'P'+str(helix)+ascii_lowercase[letter]
-				letter += 1
-			elif alden.ix[down, 2] == "HELIX" and alden.ix[down, "ie"] != "INITIATION" and alden.ix[down, "ie"] != "ELONGATION":
-				i = alden.ix[down, 3]
-				alden.ix[alden[3]==i, "ps"] = "PRIMARY"
-				alden.ix[alden[3]==i, "ie"] = "ELONGATION"
-				alden.ix[alden[3]==i, "label"] = 'P'+str(helix)+ascii_lowercase[letter]
-				letter += 1
+				alden.ix[alden[3]==i, "label1"] = 'P'+str(helix)
+				if alden.ix[up, 0] == alden.ix[up, 1]:
+					alden.ix[alden[3]==i, "label2"] = 'x'
+				else:
+					alden.ix[alden[3]==i, "label2"] = ascii_lowercase[letter]
+					letter += 1
 			up += 1
 			down -= 1
 		helix += 1
@@ -67,49 +79,60 @@ def bpseq_to_alps():
 	not_assigned = alden["ps"] != "PRIMARY"
 	sec = alden[is_helix & not_assigned]
 	stack = []
-	i = 1
-	j = 0
+	helix = 1
+	letter = 0
 	for row in sec.itertuples():
-		if len(stack) > 0 and row[7] == stack[len(stack)-1][7]:
-			alden.ix[alden[3]==row[7], "ps"] = "SECONDARY"
+		if len(stack) > 0 and row[8] == stack[len(stack)-1][8]:
+			alden.ix[alden[3]==row[8], "ps"] = "SECONDARY"
+			alden.ix[alden[3]==row[8], "label1"] = 'S'+str(helix)
 			if alden.ix[row[0]+1, 2] != "MULTISTEM" and alden.ix[row[0]+1, 2] != "FREE" and  alden.ix[stack[len(stack)-1][0]-1, 2] != "MULTISTEM" and alden.ix[stack[len(stack)-1][0]-1, 2] != "FREE":
-				if j == 0:
-					alden.ix[alden[3]==row[7], "ie"] = "INITIATION"
-					alden.ix[alden[3]==row[7], "label"] = 'S'+str(i)+ascii_lowercase[j]
-					j += 1
+				if alden.ix[row[0]+1, 2] == "BULGE" or alden.ix[row[0]+1, 2] == "INTERNAL":
+					alden.ix[row[0]+1, "label1"] = 'S'+str(helix)
+				if  alden.ix[stack[len(stack)-1][0]-1, 2] == "BULGE" or alden.ix[stack[len(stack)-1][0]-1, 2] == "INTERNAL":
+					alden.ix[stack[len(stack)-1][0]-1, "label1"] = 'S'+str(helix) 
+	 			if letter == 0:
+					alden.ix[alden[3]==row[8], "ie"] = "INITIATION"
 				else:
-					alden.ix[alden[3]==row[7], "ie"] = "ELONGATION"
-					alden.ix[alden[3]==row[7], "label"] = 'S'+str(i)+ascii_lowercase[j]
-					j += 1
+					alden.ix[alden[3]==row[8], "ie"] = "ELONGATION"
+				if row[1] == row[2]:
+					alden.ix[alden[3]==row[8], "label2"] = 'x'
+				else:
+					alden.ix[alden[3]==row[8], "label2"] = ascii_lowercase[letter]
+					letter += 1
 			else:
-				if j == 0:
-					alden.ix[alden[3]==row[7], "ie"] = "INITIATION"
-					alden.ix[alden[3]==row[7], "label"] = 'S'+str(i)+ascii_lowercase[j]
-					j = 0
+				if letter == 0:
+					alden.ix[alden[3]==row[8], "ie"] = "INITIATION"
 				else:
-					alden.ix[alden[3]==row[7], "ie"] = "ELONGATION"
-					alden.ix[alden[3]==row[7], "label"] = 'S'+str(i)+ascii_lowercase[j]
-					j = 0
-				i += 1
+					alden.ix[alden[3]==row[8], "ie"] = "ELONGATION"
+				if row[1] == row[2]:
+					alden.ix[alden[3]==row[8], "label2"] = 'x'
+				else:
+					alden.ix[alden[3]==row[8], "label2"] = ascii_lowercase[letter]
+				helix += 1
+				letter = 0
 			stack.pop()
 		else:
-			stack.append(row)
-
+			stack.append(row)	
+	
 	#Read in bpseq, skips the first 4 rows, if header is a different size change skiprows
-	#bpseq = pd.read_csv(sys.argv[1], sep=' ', header=None, skiprows=4)
+	bpseq = pd.read_csv(sys.argv[1], sep=' ', header=None, skiprows=4)
 
 	#Get sequence from bpseq
-	#seq = bpseq[1].tolist()
-	#sequence = " "
-	#for i in seq:
-	#	sequence += i
-	
+	seq = bpseq[1].tolist()
+	sequence = " "
+	for i in seq:
+		sequence += i
+	sequence = '#'+sequence
+
 	#write output
 	al2 = al + ".alps" #filename
-	alden.to_csv(al2, sep=' ', header = None, index=None)
-	
+	f = open(al2, 'a')
+	f.write(sequence)
+	alden.to_csv(f, sep=' ', header = None, index=None)
+	f.close()
+
 	#delete alden file
-	#subprocess.call(["rm", al1])
+	subprocess.call(["rm", al1])
 
 if len(sys.argv) != 2:
 	print("Need to input bpseq")
