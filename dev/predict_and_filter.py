b42037b1f7727a156661fceae342ba78be1f4b3d
Russell Durrett <russdurrett@gmail.com>
2016-04-27 17:55:51 -0500

    improvements on the prediction script;

--- dev/predict_and_filter.py
+++ dev/predict_and_filter.py
@@ -74,8 +74,8 @@ pis = df[(df.LOOPSIZE < params['maxloopsize']) & (df.LOOPSIZE >= params['minloop
 
 
 #add sequence info to potential helices 
-pis['5SEQ'] = [sq[x:y] for x,y in zip(pis['5START'].tolist(), pis['5STOP'].tolist())]
-pis['3SEQ'] = [sq[x:y] for x,y in zip(pis['3START'].tolist(), pis['3STOP'].tolist())]
+pis['5SEQ'] = [sq[x-1:y] for x,y in zip(pis['5START'].tolist(), pis['5STOP'].tolist())]
+pis['3SEQ'] = [sq[x-1:y] for x,y in zip(pis['3START'].tolist(), pis['3STOP'].tolist())]
 pis['LOOPSEQ'] = [sq[x:y-1] for x,y in zip(pis['5STOP'].tolist(), pis['3START'].tolist())]
 
 
@@ -95,8 +95,8 @@ pis['LOOP_SCORE'] = pis.apply(lambda row: row['LOOP_SCORE']+5 if (row['LOOPSEQ']
 # ADD POLE_SCORE FOR LOOP SEQUENCES PRESENT WITH FREQUENCY 
 pole_frequencies = pd.read_csv('Pole.csv')
 #If Loop Seq Contains A Sequence In LoopDF, Take Max 
-pis['POLE_SCORE'] = pis['LOOPSEQ'].apply(lambda s: pole_frequencies[pole_frequencies.apply(lambda x: x['Pole'] in s.upper(), axis=1)]['Frequency'].sum())  
-pis['POLE_SCORE'].fillna(0, inplace=True)
+pis['POLE_SCORE'] = pis['LOOPSEQ'].apply(lambda s: pole_frequencies[pole_frequencies.apply(lambda x: x['Pole'] in s.upper(), axis=1)]['Frequency'].max())  
+pis['POLE_SCORE'].fillna(pole_frequencies['Frequency'].min(), inplace=True)
 
 
 
@@ -111,15 +111,52 @@ def find_neck_score(row):
     if neck.upper() in neck_frequencies.keys(): 
         return neck_frequencies[neck.upper()]
     else: 
-        return 0.0 
+        return sorted(neck_frequencies.values())[-1] 
 
 pis['NECK_SCORE'] = pis.apply(lambda row: find_neck_score(row), axis=1)
 
 
 
+# FIND NECK SCORE FOR LAST TWO BASES PAIRED ON HELIX
+collar_frequencies = {}
+for x,y in pd.read_csv('Collar.csv').to_dict(orient='split')['data']: 
+    collar_frequencies[x]=y
+print collar_frequencies
+
+def find_collar_score(row): 
+    collar = row['LOOPSEQ'][0] + row['LOOPSEQ'][-1]
+    if collar.upper() in collar_frequencies.keys(): 
+        return collar_frequencies[collar.upper()]
+    else: 
+        return sorted(collar_frequencies.values())[-1] 
+
+pis['COLLAR_SCORE'] = pis.apply(lambda row: find_collar_score(row), axis=1)
+
+
+
+
+# FIND LOOP LENGTH SCORE
+loop_length_freq = {}
+for x,y in pd.read_csv('Length.csv').to_dict(orient='split')['data']: 
+    loop_length_freq[str(x)]=float(y)
+print loop_length_freq
+pis['LENGTH_SCORE'] = pis.apply(lambda row: loop_length_freq.get(str(row['LOOPSIZE'])+'.0', sorted(loop_length_freq.values())[0]), axis=1)
+
+
+
+
+
 # CALCULATE MODIFIED ENERGY FROM LOOP_SCORE AND LOOP SIZE 
 # pis['E_MOD'] = (pis["ENERGY"] - (pis["LOOP_SCORE"])) / pis["LOOPSIZE"]
-pis['E_MOD'] = (pis["ENERGY"] - (pis["LOOP_SCORE"]) - (pis['NECK_SCORE']) - (pis['POLE_SCORE'])) / pis["LOOPSIZE"]
+
+import math
+
+pis['WEIGHTING_FUNCTION'] = (6 * pis['ENERGY']/pis['ENERGY'].median() + (2 *  pis['NECK_SCORE']/pis['NECK_SCORE'].median()) + (pis['POLE_SCORE']/pis['POLE_SCORE'].median()) + (pis['LENGTH_SCORE']/pis['LENGTH_SCORE'].median()) + (8 * pis['COLLAR_SCORE']/pis['COLLAR_SCORE'].median())  / 10) 
+pis['E_MOD'] =  - np.log(np.exp(pis['WEIGHTING_FUNCTION'])/(1 + np.exp(pis['WEIGHTING_FUNCTION'])))
+
+# from math import exp 
+# pis['E_MOD'] = e ** -pis['E_MOD'] / (1 + e ** -pis['E_MOD'])
+
 pis = pis.sort_values(by="E_MOD")
 pis.reset_index(inplace=True, drop=True)
 
