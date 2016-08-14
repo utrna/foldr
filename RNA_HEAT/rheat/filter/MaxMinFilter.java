8db269cbab6a3b37099be68213b519f22f402a20
kevinmgrant <kmgrant@utexas.edu>
2016-08-14 14:02:26 -0700

    Removing unused code to make changes simpler.
    Large segments of commented-out code and unused files are
    being removed so it is easier to find real dependencies
    on the helix data store.

--- RNA_HEAT/rheat/filter/MaxMinFilter.java
+++ RNA_HEAT/rheat/filter/MaxMinFilter.java
@@ -43,58 +43,6 @@ extends rheat.filter.Filter {
         return this.MinLength;
     }
 
- /** 
-  * @param rna The Input RNA
-  * @return Modified RNA after application of this filter.    
-  */  
-    /*
-    public RNA apply(RNA rna) {
-        ArrayList sequence = rna.getSequence();
-        boolean[][] bp_temp = new boolean[sequence.size()][sequence.size()];            
-        boolean[][] bp_master = new boolean[sequence.size()][sequence.size()];                    
-        bp_master = rna.getBasePairs(); 
-        
-        // Creating a temp copy of base-pairs, to be used in the function
-        for (int i = 0; i < rna.getBasePairs().length; ++i) {
-            System.arraycopy(bp_master[i], 0, bp_temp[i], 0, rna.getBasePairs().length);                        
-        }
-        HelixStore OldStore = rna.getHelices();
-        HelixStore hstore = new HelixGrid(sequence.size()); 
-        //helices = rna.getHelices();
-        int helixCount = -1;
-        for (int i = 0; i< (sequence.size()-1); ++i){
-            for (int j = 0; j < i; ++j){
-                if (bp_temp[i][j] == true){
-                    int temp_i = i;
-                    int temp_j = j;
-                    int helixLength = 0;
-                    int startX = i;
-                    int startY = j;
-                    do{
-                        bp_temp[temp_i][temp_j] = false;
-                        ++temp_i; --temp_j; 
-                        ++helixLength;
-                        if (temp_i==sequence.size() || (temp_j<0))
-                            break;
-                    } while((bp_temp[temp_i][temp_j] == true));
-
-                    if ((helixLength <= MaxLength) && (helixLength >= MinLength)){
-                        Helix h = new Helix(startX, startY, helixLength);
-                        if (OldStore.hasHelix(h)) {
-                            ++helixCount;
-                            hstore.addHelix(h);
-                        }
-                    }
-                } //if
-            }//j
-        }//i        
-        System.out.println("\nTotal Number of Helices: " + helixCount);
-        rna.setHelices(hstore);
-        System.gc();
-        return rna;
-    } // apply
-    */
-    
     public RNA apply(RNA rna){
         HelixStore hs = rna.getHelices();
         HelixGrid hg = new HelixGrid(rna.getSequence().size());
